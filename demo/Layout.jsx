3ef2a89c04c6b8d20e89c625de43b4e407767023
Daniël Illouz <daniel.illouz@me.com>
2019-05-24 22:46:11 +0200

    Document basic module usage

--- demo/Layout.jsx
+++ demo/Layout.jsx
@@ -8,8 +8,52 @@ export const Main = styled.main`
   padding: 0;
 `;
 
-export const Container = styled.div`
+export const Header = styled.header`
+  text-align: center;
+`;
+
+export const Content = styled.div`
+  padding: 1em;
+  font-size: 1.25em;
+  line-height: 1.5;
+
+  @media (max-width: 520px) {
+    font-size: 1em;
+  }
+`;
+
+export const Section = styled.section`
+  width: 900px;
   margin: auto;
+
+  @media (max-width: 900px) {
+    width: 100%;
+    margin: 0;
+  }
+`;
+
+export const Interactive = styled.div`
+  margin: 3em 0;
+  font-size: 1rem;
+
+  @media (max-width: 520px) {
+    font-size: 0.8rem;
+  }
+`;
+
+export const Caption = styled.div`
+  text-align: center;
+  font-size: 0.9rem;
+  color: #aaa;
+
+  @media (max-width: 520px) {
+    font-size: 0.7rem;
+  }
+`;
+
+export const Container = styled.div`
+  width: 475px;
+  margin: 1em auto;
   padding: 1em;
   background-color: #fff;
   color: #333;
@@ -18,8 +62,7 @@ export const Container = styled.div`
   text-align: center;
   box-shadow: 0 2px 4px #00000018;
 
-  @media (max-width: 460px) {
+  @media (max-width: 520px) {
     width: 100%;
-    border-radius: 0;
   }
 `;
--- demo/request.js
+++ demo/request.js
@@ -1,12 +1,14 @@
 export function fakeRequest(data) {
-  console.log('fake request with data: ', data); // eslint-disable-line no-console
-
+  // eslint-disable-next-line no-unused-vars
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       // Uncomment below to trigger error:
       // return reject('Error: KABOOM!');
 
-      resolve();
+      resolve({
+        status: 'ok',
+        scheduled: data
+      });
     }, 2e3);
   });
 }
