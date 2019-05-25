ca5d73612f20beb271c0290ffebb8afacfb25287
Daniël Illouz <daniel.illouz@me.com>
2019-05-25 19:14:06 +0200

    Add footer

--- demo/components/Layout.jsx
+++ demo/components/Layout.jsx
@@ -7,3 +7,11 @@ export const Main = styled.main`
   margin: 0;
   padding: 0;
 `;
+
+export const Footer = styled.footer`
+  margin: 1em 0;
+  padding: 2em 1em;
+  text-align: center;
+  font-size: 1em;
+  border-top: 1px solid #f0f0f0;
+`;
